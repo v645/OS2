d13f04bba9408187926b9ae6fc8900b2fd311a71
v645 <45901718+v645@users.noreply.github.com>
2020-10-21 17:12:42 +0300

    upload

---
+++ consoleWriter_P1/consoleWriter_P1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+    cout << "console start" << endl;
+    HANDLE mutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, TEXT("MainProcessMutex"));
+
+    HANDLE fileMapping = OpenFileMapping(FILE_MAP_READ, FALSE, TEXT("FileMapping"));
+    HANDLE sizeMapping = OpenFileMapping(FILE_MAP_READ, FALSE, TEXT("SizeMapping"));
+
+
+    //cout << "console 2::size_!=" <<nums.size() << endl;
+
+    int currentWritten = 0;
+
+    while (true)
+    {
+        cout << "xc"<<endl;
+         WaitForSingleObject(mutex, 100);
+        int* sizeM = (int*)MapViewOfFile(sizeMapping, FILE_MAP_READ, 0, 0, 0);
+        int* ch = (int*)MapViewOfFile(fileMapping, FILE_MAP_READ, 0, 0, 0);
+
+        int size = *sizeM;
+        cout << "size=" << size << endl;
+       // cout << "sizeof(ch)=" << sizeof(*ch) << endl;
+        vector<int> nums=(vector<int>)*(ch);
+       // vector<int> nums=vector<int>(ch,ch+size);
+        /*
+        cout << "nums count=" << nums.size() << endl;
+        cout << "ch[0]=" << ch[0] << endl;
+        cout << "ch[1]=" << ch[1] << endl;
+        cout << "ch[2]=" << ch[2] << endl;
+        cout << "ch[3]=" << ch[3] << endl;*/
+
+        if (currentWritten < size)
+
+        {
+            // cout <<"CONSOLE:"<< ch[currentWritten++] << endl;
+            currentWritten++;
+            //cout <<"CONSOLE n:"<< nums[currentWritten] << endl;
+            cout << "CONSOLE c[" << (currentWritten - 1) << "]" << ":--> " << ch[currentWritten - 1] << endl;
+           // cout << "CONSOLE n[" << (currentWritten - 1) << "]" << ":--> " << nums[currentWritten - 1] << endl;
+            // cout <<"CONSOLE:"<< *ch << endl;
+        }
+
+        UnmapViewOfFile(ch);
+        UnmapViewOfFile(sizeM);
+       ReleaseMutex(mutex);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+}
---
+++ fileWriter_P2/fileWriter_P2.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <thread>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+int main1()
+{
+    HANDLE mutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, TEXT("MainProcessMutex"));
+
+    HANDLE fileMapping = OpenFileMapping(FILE_MAP_WRITE, FALSE, TEXT("FileMapping"));
+
+    char* ch = (char*)MapViewOfFile(fileMapping, FILE_MAP_WRITE, 0, 0, 0);
+    while (true)
+    {
+        WaitForSingleObject(mutex, INFINITE);
+        cout << "FP" << endl;
+
+        ofstream out("D:\\hello.txt", std::ios::app);
+        if (out.is_open())
+        {
+            out << *ch << std::endl;
+            out << ch << std::endl;
+            out << &ch << std::endl;
+        }
+        out.close();
+        ReleaseMutex(mutex);
+    }
+}
+
+
+int main()
+{
+    cout << "cfile start" << endl;
+    HANDLE mutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, TEXT("MainProcessMutex"));
+
+    HANDLE fileMapping = OpenFileMapping(FILE_MAP_READ, FALSE, TEXT("FileMapping"));
+    HANDLE sizeMapping = OpenFileMapping(FILE_MAP_READ, FALSE, TEXT("SizeMapping"));    
+
+    int currentWritten = 0;
+
+    while (true)
+    {
+        cout << "fxc"<<endl;
+       WaitForSingleObject(mutex, 110);
+
+        int* sizeM = (int*)MapViewOfFile(sizeMapping, FILE_MAP_READ, 0, 0, 0);
+        int* ch = (int*)MapViewOfFile(fileMapping, FILE_MAP_READ, 0, 0, 0);
+        int size = *sizeM;
+                cout << "write in file"<< ch[currentWritten - 1];
+        if (currentWritten < size)
+        {
+            ofstream out("D:\\hello.txt", std::ios::app);
+            if (out.is_open())
+            {
+                out << ch[currentWritten - 1] << std::endl;
+            }
+            out.close();
+        }
+        UnmapViewOfFile(sizeM);
+        UnmapViewOfFile(ch);
+       ReleaseMutex(mutex);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
